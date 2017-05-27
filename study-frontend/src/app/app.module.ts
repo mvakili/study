6002e193952798b73d7b4b3d5f89aae77aaa6275
mvakili <m.vakili95@gmail.com>
2017-05-27 06:53:15 +0430

    #16 ToastController added to application to display errors and messages

--- study-frontend/src/app/app.module.ts
+++ study-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, ToastController } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
@@ -32,6 +32,7 @@ import { LoginPage } from '../pages/login-page/login-page';
   providers: [
     StatusBar,
     SplashScreen,
+    ToastController,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
