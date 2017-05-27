5afc09d32be34d74703296d7a293e239d65b2d73
mvakili <m.vakili95@gmail.com>
2017-05-27 06:57:07 +0430

    #16 Display validation message on signup email validation

--- study-frontend/src/pages/login-page/login-page.ts
+++ study-frontend/src/pages/login-page/login-page.ts
@@ -1,8 +1,9 @@
 import { Component, trigger, state, style, transition, animate, keyframes, ViewChild } from '@angular/core';
-import { NavController, LoadingController, Grid, Slides } from 'ionic-angular';
+import { NavController, LoadingController, Grid, Slides, ToastController, Toast } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import {Http, Headers, RequestOptions} from '@angular/http';
 import {Account} from './../../services';
+import { ResultStatus} from './../../communication';
 
 @Component({
   selector: 'page-login',
@@ -14,7 +15,7 @@ export class LoginPage {
   email: string = '';
 
 
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, storage: Storage, public http: Http) {
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, storage: Storage, public http: Http, public toastCtrl: ToastController) {
     storage.ready().then(() => {
     })
   }
@@ -34,17 +35,44 @@ export class LoginPage {
   }
 
     public validateEmail() {
+    let toast: Toast;
+    let toasts = Array<Toast>();
     let loader = this.loadingCtrl.create({
       content: "Please wait..."
     })
-    loader.present();
-
     let account = new Account(this.http);
+    
+    loader.present();
+    
+
     account.emailValidate(this.email).then(data => {
       loader.dismiss();
-      
+
+      if(data.ResultStatus != ResultStatus.Successful)
+      {
+        data.Errors.forEach(element => {
+          toast = this.toastCtrl.create({
+            duration: 3000,
+            position: 'bottom',
+            message: element
+          });
+          toast.present();
+        });
+      } else {
+          toast = this.toastCtrl.create({
+            duration: 3000,
+            position: 'bottom',
+            message: 'Email is valid'
+          });
+          toast.present();
+      }
     }).catch(err => {
       loader.dismiss();
+      this.toastCtrl.create({
+        duration: 3000,
+        position: 'bottom',
+        message: 'Couldn\'t connect to server'
+      }).present();
     });
     
   }
